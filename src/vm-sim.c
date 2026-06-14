e317d7148d448e98e65988125a8b439d96eca7b3
Brian Railing <bpr@cs.cmu.edu>
2026-06-14 00:36:24 -0400

    Add code and remove \r\n

--- src/vm-sim.c
+++ src/vm-sim.c
@@ -29,9 +29,70 @@ int loadLibrary(char* fileName)
     return 0;
 }
 
-int main(int argc, char** argv)
+int frameCount = -1;
+uint32_t allocateFrame(void)
+{
+    // Frame 0 is the root of the page table
+    static uint32_t nextFrame = 0;
+    if (nextFrame < (uint32_t)frameCount ||
+        frameCount == -1)
+    {
+        return ++nextFrame;
+    }
+    
+    return 0;
+}
+
+uint32_t allocateSwap(void)
+{
+    static uint32_t nextSwap = 0;
+    
+    // TODO - swap needs actual memory so that contents can be copied
+    
+    return ++nextSwap;
+}
+
+void copyToSwap(uint32_t frame, uint32_t swap)
 {
     
+}
+
+void copyFromSwap(uint32_t swap, uint32_t frame)
+{
+    
+}
+
+// vm-sim -t <trace> -o <page fault library> -p <page size> -V <verbose level> -n <frame count>
+int main(int argc, char** argv)
+{
+    char* traceName = NULL;
+    char* libName = NULL;
+    
+    int pageSize = 256;
+    
+    // Get opt of arguments
+    
+    // open page fault library
+    if (loadLibrary(libName) != 0)
+    {
+        
+    }
+    struct config c;
+    c.pageSize = pageSize;
+    c.numFrames = frameCount;
+    c.allocateFrame = allocateFrame;
+    c.allocateSwap = allocateSwap;
+    c.copyFromSwap = copyFromSwap;
+    c.copyToSwap = copyToSwap;
+    initLibrary(&c);
+    
+    // open trace
+    FILE* trace = fopen(traceName, "r");
+    int addr = -1;
+    while (fscanf(trace, "%x\n", &addr) > 0)
+    {
+        
+    }
     
     return 0;
 }
